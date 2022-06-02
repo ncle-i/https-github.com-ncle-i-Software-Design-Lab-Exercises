38a71edad100d9b98e6d26622a46c35b2b641199
ncle-i <106762066+ncle-i@users.noreply.github.com>
2022-06-03 07:49:15 +0800

    Add files via upload

---
+++ Lab3-Inlab-Singular.py
@@ -0,0 +1,14 @@
+NicoleAnne = {0, 22, 33, 55, 99};
+Mejia= {22, 33, 55, 99, 100};
+
+# union
+print("\nThe union between the two sets:", NicoleAnne| Mejia)
+
+# intersection
+print("\nThe intersection of two sets:", NicoleAnne & Mejia)
+
+# difference
+print("\nThe difference between two sets:", NicoleAnne - Mejia)
+
+# symmetric difference
+print("\nThe symmetric difference:", NicoleAnne ^ Mejia)
