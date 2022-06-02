ba032e2825856ab87ff66d383c9a1a8e923389fc
ncle-i <106762066+ncle-i@users.noreply.github.com>
2022-06-03 07:54:02 +0800

    Add files via upload

---
+++ Lab3Inlab2A.py
@@ -0,0 +1,13 @@
+stack=[]
+
+stack.append("Nicole")
+stack.append("Anne")
+stack.append("Mejia")
+
+print('Initial stack')
+print(stack)
+
+print('\nElements popped from stackk:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
